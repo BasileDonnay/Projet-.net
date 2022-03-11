2dff6924096fec60246cd1b9af28c22e4b964401
Base <bdonnay@helb-prigogine.be>
2022-03-11 09:59:02 +0100

    meilleur système de saut

--- RunGun/Assets/Scripts/CharacterController2D.cs
+++ RunGun/Assets/Scripts/CharacterController2D.cs
@@ -5,35 +5,57 @@ using UnityEngine;
 public class CharacterController2D : MonoBehaviour
 {
     [SerializeField] private float x_addition = 0.01f;
-    [SerializeField] private float y_addition = 0.01f;
     [SerializeField] private string left = "q";
     [SerializeField] private string right = "d";
     [SerializeField] private string up = "z";
-    [SerializeField] private string down = "s";
+    [SerializeField] private int jumpForce = 200;
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+    private bool isGrounded = true;
+    private bool jumpCooldown = false;
+    public int jCConst = 50;
+    private int jCCounter = 0;
+    public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        jCCounter = jCConst;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(right)) {
+        if (Input.GetKey(right))
+        {
             transform.position = transform.position + new Vector3(x_addition, 0, 0);
         }
         if (Input.GetKey(left))
         {
             transform.position = transform.position - new Vector3(x_addition, 0, 0);
         }
-        if (Input.GetKey(up))
+
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+        updateJumpCooldown();
+
+        if (Input.GetKey(up) && isGrounded && jumpCooldown)
         {
-            transform.position = transform.position + new Vector3(0, y_addition, 0);
+            rb.AddForce(new Vector2(0f, jumpForce));
+            jumpCooldown = false;
         }
-        if (Input.GetKey(down))
+    }
+
+    private void updateJumpCooldown()
+    {
+        if (jumpCooldown == false)
         {
-            transform.position = transform.position - new Vector3(0, y_addition, 0);
+            jCCounter--;
+            if (jCCounter <= 0)
+            {
+                jumpCooldown = true;
+                jCCounter = jCConst;
+            }
         }
     }
 }
