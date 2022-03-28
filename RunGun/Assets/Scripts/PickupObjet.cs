7f99fc7f83f509597ba7d87a7beccdd575f9aa06
MichelSow <99797863+MichelSow@users.noreply.github.com>
2022-03-28 22:58:01 +0200

    system de piece

---
+++ RunGun/Assets/Scripts/PickupObjet.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class PickupObjet : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision){
+
+ if( collision.CompareTag("Player")){
+
+Inventory.instance.AddCoins(1);
+     Destroy(gameObject);
+ }
+
+
+
+    }
+}
