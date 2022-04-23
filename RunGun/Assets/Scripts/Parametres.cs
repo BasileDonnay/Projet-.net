861f8143e647113b9f95f3357238b3da51ed229c
Fred1903 <100136262+Fred1903@users.noreply.github.com>
2022-04-23 18:03:19 +0200

    Amelioration bouton parametre dans le jeu
    Quand on appuie sur le bouton parametre en jeu, le jeu se met en pause

--- RunGun/Assets/Scripts/Parametres.cs
+++ RunGun/Assets/Scripts/Parametres.cs
@@ -3,6 +3,20 @@ using UnityEngine.Audio;
 
 public class Parametres : MonoBehaviour
 {
+    [SerializeField] GameObject ParametresJeu;
+
+    public void PauseParametres()
+    {
+        ParametresJeu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Reprendre()
+    {
+        ParametresJeu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
     public AudioMixer audioMixer;
 
     public void SetVolume(float volume)
