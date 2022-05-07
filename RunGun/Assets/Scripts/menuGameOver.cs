b50b00378148a74efacbebb22be49f05ce24fdf2
Fred1903 <100136262+Fred1903@users.noreply.github.com>
2022-05-07 20:50:33 +0200

    Avancement menu GameOver
    Maiis je ne sais pas ce que cela donne car le jeu ne fonctionne pas la

--- RunGun/Assets/Scripts/menuGameOver.cs
+++ RunGun/Assets/Scripts/menuGameOver.cs
@@ -1,10 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class menuGameOver : MonoBehaviour
 {
-    public void menu_GameOver(){
-        gameObject.SetActive(true);
+    public GameObject gameOverUI;
+    public string niveauACharger;
+
+    private void OnEnable()
+    {
+        Health.MortPerso += AfficheMenuGO;
+    }
+
+    private void OnDisable()
+    {
+        Health.MortPerso -= AfficheMenuGO;
+    }
+   
+    public void AfficheMenuGO()
+    {
+        gameOverUI.SetActive(true);
+        Time.timeScale = 0f; // permet que lorsqu'on appuie sur pause, le jeu s'arrete aussi et ne continue pas
+    }
+
+    public void RecommencerJeu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(niveauACharger);
+    }
+
+    public void RetournerM(int sceneID)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneID);
     }
 }
