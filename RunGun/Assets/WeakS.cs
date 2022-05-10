feea603810d14def57215911d295a96f71ab3e80
Younes210 <100136250+Younes210@users.noreply.github.com>
2022-05-10 14:05:54 +0200

    Suite Serpent

---
+++ RunGun/Assets/WeakS.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+public class WeakS : MonoBehaviour
+{
+    public GameObject objectToDestroy;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(objectToDestroy);
+        }
+    }
+}
